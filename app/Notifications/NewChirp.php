357e04cd3655d00f23738b4b5cfa057f1d2dfff9
Lee Walker <lee@codejourneymen.com>
2024-07-10 18:43:33 -0400

    Remove Str, as it was the problem.
    Signed-off-by: Lee Walker <lee@codejourneymen.com>

--- app/Notifications/NewChirp.php
+++ app/Notifications/NewChirp.php
@@ -39,7 +39,7 @@ class NewChirp extends Notification
         return (new MailMessage)
             ->subject("New Chirp from {$this->chirp->user->name}")
             ->greeting("New Chirp from {$this->chirp->user->name}")
-            ->line(Str::limit($this->chirp->message, 50))
+            ->line($this->chirp->message)
             ->action('Go to Chirper', url('/'))
             ->line('Thank you for using our application!');
     }
